b47724c0d709f65ee1411d46e7a7606bcd8990a5
dravgalis <artem.dravgalis@gmail.com>
2026-04-06 14:19:55 +0300

    Fix tooltip stacking order above score badges

--- project/app/src/main.tsx
+++ project/app/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { AppRouter } from './AppRouter';
 import './styles.css';
 
+document.body.classList.add('theme-default');
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AppRouter />
