a12af7b8826238fdd9bfd7ce7b033e2c4c267e46
dravgalis <artem.dravgalis@gmail.com>
2026-04-07 17:11:02 +0300

    Switch admin store to DATABASE_URL with Postgres support

--- project/admin/src/AppRouter.tsx
+++ project/admin/src/AppRouter.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { ADMIN_ROUTES } from './config';
 import { AdminDashboardPage } from './pages/AdminDashboardPage';
 import { AdminLoginPage } from './pages/AdminLoginPage';
+import { AdminUserDetailsPage } from './pages/AdminUserDetailsPage';
 
 export function AppRouter() {
   return (
@@ -9,6 +10,7 @@ export function AppRouter() {
       <Routes>
         <Route path={ADMIN_ROUTES.login} element={<AdminLoginPage />} />
         <Route path={ADMIN_ROUTES.dashboard} element={<AdminDashboardPage />} />
+        <Route path="/admin/users/:hhId" element={<AdminUserDetailsPage />} />
         <Route path="*" element={<Navigate to={ADMIN_ROUTES.login} replace />} />
       </Routes>
     </BrowserRouter>
